43a6a21fdeb38bc469ddc52ad6c7840c9e368db9
tsgan <ganbold@gmail.com>
2013-10-03 19:43:04 +0800

    comment devmap lines for now

--- rk30xx_machdep.c
+++ rk30xx_machdep.c
@@ -82,7 +82,7 @@ int
 platform_devmap_init(void)
 {
 	int i = 0;
-
+/*
 	fdt_devmap[i].pd_va = 0xFEA00000;
 	fdt_devmap[i].pd_pa = 0x10000000;
 	fdt_devmap[i].pd_size = 0x100000;
@@ -103,7 +103,7 @@ platform_devmap_init(void)
 	fdt_devmap[i].pd_prot = VM_PROT_READ | VM_PROT_WRITE;
 	fdt_devmap[i].pd_cache = PTE_DEVICE;
 	i++;
-
+*/
 	fdt_devmap[i].pd_va = 0xFED00000;
 	fdt_devmap[i].pd_pa = 0x20000000;
 	fdt_devmap[i].pd_size = 0x100000;
